c0c33ccb0f4292d02a5c3ad99ebe6821a9f8d3e9
tbayetird <theophile.bayet@ird.fr>
2019-05-20 11:39:17 +0000

    datas for starting

---
+++ test_basics.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+
+
+
+##  Fonction d'ouverture d'image
+#
+#  Paramètres :
+# - image_path : chemin vers l'image à ouvrir
+#
+# Return :
+# - l'image dont le chemin est fourni
+def open_image(image_path):
+
+    return 0;
+
+##  Fonction d'ouverture de video
+#
+#  Paramètres :
+# - video_path : chemin vers la vidéo à ouvrir
+#
+# Return :
+# - la video dont le chemin est fourni
+def open_video(video_path):
+    
+    return 0;
+
+
+##  Fonction de sauvegarde d'image
+#
+#  Paramètres :
+# - image : l'image à sauvegarder
+# - name : le nom à donner à l'image sauvegardée
+def save_image(image,name):
+    pass
